b1cb4ebe04088ffc5a4ca1f093c87545571e97aa
gabriel <gabrielfranksimonetto@gmail.com>
2020-03-30 20:45:37 -0300

    dependency hell achieved

---
+++ dependency_hell/demo2.py
@@ -0,0 +1,4 @@
+from dependency_hell.my_dir import gibe_data
+
+data = gibe_data()
+print(data)
--- dependency_hell/my_dir/__init__.py
+++ dependency_hell/my_dir/__init__.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from data import gibe_data
 
 PROJECT_ROOT = Path(__file__).parents[2]
 DATAPATH = PROJECT_ROOT.joinpath('data')
